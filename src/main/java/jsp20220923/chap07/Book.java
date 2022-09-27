aaf6b43e89464ae726fe0fee1b165fbfc654660e
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-09-27 17:04:09 +0900

    프로퍼티 작성★★★

---
+++ src/main/java/jsp20220923/chap07/Book.java
@@ -0,0 +1,31 @@
+package jsp20220923.chap07;
+
+public class Book {
+	private String title;
+	private Integer price;
+
+	public Book(String title, Integer price) {
+		super();
+		this.title = title;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+}
+
+
