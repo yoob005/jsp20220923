3bacedc7db661c5a100d3308c628616ace505313
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-10-11 18:20:48 +0900

    filter

---
+++ src/main/java/filter/chap19/Filter01.java
@@ -0,0 +1,52 @@
+package filter.chap19;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class Filter01
+ */
+@WebFilter("/Servlet11")
+public class Filter01 implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Filter01() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		// filter가 해야하는 일 작성
+		System.out.println("필터1번 일함.");
+		// pass the request along the filter chain
+		// 필터 체인에 요청을 계속 전달한다.(다음 필터/서블릭 실행)
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
